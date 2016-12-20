f0cb8fdba38ed1bbafac64f92a1b5521c54a8ef8
Svetlichnyy AV <usbania@gmail.com>
2016-12-20 21:00:38 +0300

    Push

--- git-describer/app/Main.hs
+++ git-describer/app/Main.hs
@@ -1,6 +1,21 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+import           Network.HTTP.Client      (newManager)
+import           Network.HTTP.Client.TLS  (tlsManagerSettings)
+import           Web.Telegram.API.Bot
+import           Web.Telegram.API.Bot.Responses
+
+
 
 main :: IO ()
-main = someFunc
+main = do
+  manager <- newManager tlsManagerSettings
+  res <- getMe token manager
+  case res of
+    Left e -> do
+      putStrLn "Request failed"
+      print e
+    Right (Response u) -> do
+      putStrLn "Request succeded"
+      print $ user_first_name u
+  where token = Token "bot281687116:AAHGdr5AP7_96pE-75_UoQLILZqGRzMeUkg" -- entire Token should be bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
