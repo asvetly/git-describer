c1e81df6543c03f9ef62096719ee122c304d5660
Svetlichnyy AV <usbania@gmail.com>
2016-12-20 22:07:15 +0300

    sendMsg

--- git-describer/app/Main.hs
+++ git-describer/app/Main.hs
@@ -5,17 +5,22 @@ import           Network.HTTP.Client.TLS  (tlsManagerSettings)
 import           Web.Telegram.API.Bot
 import           Web.Telegram.API.Bot.Responses
 
-
-
-main :: IO ()
-main = do
+getMsg = do
   manager <- newManager tlsManagerSettings
-  res <- getMe token manager
+  let request = sendMessageRequest chatId message
+  res <- sendMessage token request manager
   case res of
     Left e -> do
       putStrLn "Request failed"
       print e
-    Right (Response u) -> do
+    Right (Response m) -> do
       putStrLn "Request succeded"
-      print $ user_first_name u
+      print $ message_id m
+      print $ text m
+  getMsg
   where token = Token "bot281687116:AAHGdr5AP7_96pE-75_UoQLILZqGRzMeUkg" -- entire Token should be bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
+        chatId = "205838843"
+        message = "Данька лах)))0))"
+
+main :: IO ()
+main = getMsg
