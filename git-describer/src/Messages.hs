ff70a9fe8618092af10c55c4eb64eeb1c18669b4
Svetlichnyy AV <usbania@gmail.com>
2016-12-22 20:57:05 +0300

    To lower case messages, code review

--- git-describer/src/Messages.hs
+++ git-describer/src/Messages.hs
@@ -6,9 +6,10 @@ module Messages
       size,
       main_loop,
       getMsg,
-      sendMsg,
       getMsgFunction,
-      processing_updates,
+      get_update_id,
+      get_text,
+      sendMsg,
       get_chat_id
     ) where
 
@@ -19,56 +20,70 @@ import           Web.Telegram.API.Bot.Responses
 import           Data.Text
 import           Data.List as List
 import           Parser
-import			 Processing
+
 
 token :: Token
 token = Token $ pack "bot281687116:AAHGdr5AP7_96pE-75_UoQLILZqGRzMeUkg"
 
+
 size :: Maybe Int
 size = Just 20
 
+
 main_loop :: [Command] -> IO()
 main_loop cmds = do
-   getMsg Nothing cmds
-   main_loop cmds    
+  getMsg Nothing cmds
+  main_loop cmds
+
 
 getMsg :: Maybe Int -> [Command] -> IO()
 getMsg id cmds = do
   manager <- newManager tlsManagerSettings
-  res <- getUpdates token id size Nothing manager
+  res     <- getUpdates token id size Nothing manager
   case res of
-    Left e -> do
-      putStrLn "Request failed"
-      print e
-    Right (Response m) -> do
-      getMsgFunction m cmds
+      Left e             -> putStrLn "Request failed" >> print e
+      Right (Response m) ->  getMsgFunction m cmds
+
 
 sendMsg :: Text -> Text -> IO()
 sendMsg message chatId = do
-  manager <- newManager tlsManagerSettings
   let request = sendMessageRequest chatId message
-  res <- sendMessage token request manager
+  manager <- newManager tlsManagerSettings
+  res     <- sendMessage token request manager
   case res of
-    Left e -> do
-      putStrLn "Request failed"
-      print e
-    Right (Response m) -> do putStrLn "Message sent"
+      Left e             -> putStrLn "Request failed" >> print e
+      Right (Response m) -> putStrLn "Message sent"
 
 
 getMsgFunction :: [Update] -> [Command] -> IO ()
 getMsgFunction [] _   = do putStr ""
 getMsgFunction m cmds = do
-    processing_updates m cmds
-    getMsg next_id cmds
- 	where next_id = Just ((update_id (List.last m)) + 1)
+    processing m cmds
+    getMsg (Just ((get_update_id m) + 1)) cmds
 
-processing_updates :: [Update] -> [Command] -> IO ()
-processing_updates [] _        = putStr ""
-processing_updates (x:xs) cmds = do
-    sendMsg (get_message x cmds) (get_chat_id x)
-    processing_updates xs cmds
+
+processing :: [Update] -> [Command] -> IO ()
+processing [] _        = putStr ""
+processing (x:xs) cmds = do
+    sendMsg msg (get_chat_id x)
+    processing xs cmds
+    where
+      msg = case (descByTitle (unpack $ toLower $ get_text x) cmds) of
+        Just desc -> pack $ desc
+        otherwise -> "Error 404: command not found"
+
+
+get_update_id :: [Update] -> Int
+get_update_id m = update_id (List.last m)
+
+
+get_text :: Update -> Text
+get_text x = get_text' (message x)
+    where get_text'  (Just mess) = get_text'' (text mess)
+          get_text'' (Just x)    = x
 
 
 get_chat_id :: Update -> Text
 get_chat_id x = get_chat_id' (message x)
-    where get_chat_id' (Just mess) = pack $ show $ chat_id (chat mess)
+    where
+      get_chat_id' (Just mess) = pack $ show $ chat_id (chat mess)
--- git-describer/src/Processing.hs
+++ git-describer/src/Processing.hs
@@ -1,4 +1,3 @@
-
 module Processing
     (
     	get_text,
@@ -7,17 +6,19 @@ module Processing
     	delete_extra_symbols
     ) where
 
-import           Web.Telegram.API.Bot
-import           Web.Telegram.API.Bot.Responses
-import           Data.Text as Text
-import			 Parser
-import			 Data.Char as Char
-import			 Data.List as List
+import                   Web.Telegram.API.Bot
+import                   Web.Telegram.API.Bot.Responses
+import                   Data.Text as Text
+import        			     Parser
+import        			     Data.Char as Char
+import        			     Data.List as List
+
 
 get_text :: Update -> Text
 get_text x = get_text' (message x)
-    where get_text' (Just mess) = get_text'' (text mess)
-          get_text'' (Just x) = x
+  where get_text'  (Just mess) = get_text'' (text mess)
+        get_text'' (Just x)    = x
+
 
 get_message :: Update -> [Command] -> Text
 get_message x cmds = case (descByTitle (input_processing x) cmds) of
@@ -28,6 +29,7 @@ get_message x cmds = case (descByTitle (input_processing x) cmds) of
 input_processing :: Update -> String
 input_processing x = List.map Char.toLower $ delete_extra_symbols (unpack $ get_text x)
 
+
 delete_extra_symbols :: String -> String
 delete_extra_symbols = List.reverse . List.dropWhile notLetter . List.reverse . List.dropWhile notLetter
-              where notLetter x = not $ isLetter x
+                       where notLetter x = not $ isLetter x
